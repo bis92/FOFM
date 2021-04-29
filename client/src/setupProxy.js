a3049d46504500b8c49353e6d172da72b739b9ce
Inseob Byun <bis920309@mail.google.com>
2021-04-30 03:16:42 +0900

    eb deploy by docker5

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,12 +5,8 @@ module.exports = function (app) {
     "/api",
     createProxyMiddleware({
       target: "http://localhost:5000",
-      changeOrigin: true,
+      changeOrigin: true
     })
   );
-  app.use(proxy("/auth/google", { target: "http://localhost:3000" }));
 };
 
-module.exports = function(app) {
-  
-};
